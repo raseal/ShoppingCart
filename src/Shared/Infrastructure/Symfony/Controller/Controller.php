6a1592a1560bf55ee0265523adf3a4869971427a
raseal <ramon.serra@uvinum.com>
2021-05-15 18:55:25 +0200

    Create sync query bus and add it to the parent Symfony controller

--- src/Shared/Infrastructure/Symfony/Controller/Controller.php
+++ src/Shared/Infrastructure/Symfony/Controller/Controller.php
@@ -7,6 +7,9 @@ namespace Shared\Infrastructure\Symfony\Controller;
 use Ramsey\Uuid\Uuid;
 use Shared\Domain\Bus\Command\Command;
 use Shared\Domain\Bus\Command\CommandBus;
+use Shared\Domain\Bus\Query\Query;
+use Shared\Domain\Bus\Query\QueryBus;
+use Shared\Domain\Bus\Query\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use function json_decode;
@@ -14,12 +17,18 @@ use function json_decode;
 abstract class Controller extends AbstractController
 {
     public function __construct(
-        protected CommandBus $command_bus
+        protected CommandBus $command_bus,
+        protected QueryBus $query_bus
     ) {}
 
     protected function dispatch(Command $command): void
     {
         $this->command_bus->dispatch($command);
+    }
+
+    protected function ask(Query $query): Response
+    {
+        return $this->query_bus->ask($query);
     }
 
     protected function getPayloadAsArray(Request $request): array
