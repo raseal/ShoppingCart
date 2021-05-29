1142471cbec0e26bdc808a865cc30b36085cdbc5
raseal <ramon.serra@uvinum.com>
2021-05-29 15:05:14 +0200

    Create migration for `cart` table

---
+++ apps/Symfony/migrations/Version20210529124932.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210529124932 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create `cart` table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(
+            'CREATE TABLE cart(
+                id BINARY(16) NOT NULL,
+                created DATETIME NOT NULL DEFAULT NOW(),
+                total DECIMAL (5,2) NOT NULL,
+                PRIMARY KEY(id)
+            )
+            DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`
+            ENGINE = InnoDB'
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE cart');
+    }
+}
